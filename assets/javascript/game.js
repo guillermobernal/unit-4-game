9344f46e0b3d2426c74bdb2fe736d2d5f4a89739
guillermobernal <43592420+guillermobernal@users.noreply.github.com>
2019-04-22 20:07:21 -0500

    re creating github repo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,54 @@
 $(document).ready(function() {
     var scoreToGet;
-    var wins=0;
-    var loses=0;
-    var yourScore=0;
+    var wins = 0;
+    var loses = 0;
+    var randomNumber;
 
     var crysOne = $("#crystal1");
+        //crysOne = 0;
     var crysTwo = $("#crystal2");
     var crysThree = $("#crystal3");
     var crysFour = $("#crystal4");
+
+    var yourScore = function(){
+        randomNumber = Math.floor(Math.random() *12) + 1;
+        console.log(randomNumber)
+
+    
     
     $("#crystal1").on("click", function(){
-        crysOne = Math.floor(Math.random()*12)+1;
+        crysOne += yourScore;
         console.log(crysOne);
     })
 
-    //repeat ^^ 3 times - once for each crystal image
+    $("#crystal2").on("click", function(){
+        crysTwo = Math.floor(Math.random()*12)+1;
+        console.log(crysTwo);
+    })
+
+    $("#crystal3").on("click", function(){
+        crysThree = Math.floor(Math.random()*12)+1;
+        console.log(crysThree);
+    })
+
+    $("#crystal4").on("click", function(){
+        crysFour = Math.floor(Math.random()*12)+1;
+        console.log(crysFour);
+    });
+    }
+
 
     //generates random score to match
 
-    $("#crystal1").on("click", function(){
-         = Math.floor(Math.random()*12)+1;
-        console.log(crysOne);
+    var game = function () {
+        scoreToGet = Math.floor(Math.random()*102)+19;
+        console.log(scoreToGet);
+    $("#scoreMatch").text("Match This Number: " + scoreToGet);
+    
+    }
+
+
+    game();
     })
     
     
-})
